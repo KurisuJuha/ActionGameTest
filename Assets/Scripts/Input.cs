8ec2cb8c12ca426d6c2859d92b69dc500b262a1c
KYapp <kukiyomenaisapp@gmail.com>
2023-03-07 23:16:32 +0900

    各キーにdownとupを追加1

--- Assets/Scripts/Input.cs
+++ Assets/Scripts/Input.cs
@@ -1,31 +1,44 @@
-using UnityInput = UnityEngine.Input;
-using KeyCode = UnityEngine.KeyCode;
+using UnityEngine;
 
 namespace ActionGameTest
 {
-    public class Input
+    public class GameInput
     {
         public bool up { get; private set; }
+        public bool upUp { get; private set; }
+        public bool upDown { get; private set; }
+
         public bool right { get; private set; }
+        public bool rightUp { get; private set; }
+        public bool rightDown { get; private set; }
+
         public bool left { get; private set; }
+        public bool leftUp { get; private set; }
+        public bool leftDown { get; private set; }
 
         public void Update()
         {
-            ResetInputs();
+            bool newUp = false;
+            bool newRight = false;
+            bool newLeft = false;
 
-            up |= UnityInput.GetKey(KeyCode.UpArrow);
-            up |= UnityInput.GetKey(KeyCode.W);
-            right |= UnityInput.GetKey(KeyCode.RightArrow);
-            right |= UnityInput.GetKey(KeyCode.D);
-            left |= UnityInput.GetKey(KeyCode.LeftArrow);
-            left |= UnityInput.GetKey(KeyCode.A);
-        }
+            newUp |= Input.GetKey(KeyCode.UpArrow);
+            newUp |= Input.GetKey(KeyCode.W);
+            newRight |= Input.GetKey(KeyCode.RightArrow);
+            newRight |= Input.GetKey(KeyCode.D);
+            newLeft |= Input.GetKey(KeyCode.LeftArrow);
+            newLeft |= Input.GetKey(KeyCode.A);
 
-        private void ResetInputs()
-        {
-            up = false;
-            right = false;
-            left = false;
+            upUp = up && !newUp;
+            upDown = !up && newUp;
+            rightUp = right && !newRight;
+            rightDown = !right && newRight;
+            leftUp = left && !newLeft;
+            leftDown = !left && newLeft;
+
+            up = newUp;
+            right = newRight;
+            left = newLeft;
         }
     }
 }
