b4712d982b2769bc0530122045984339d67cfeca
KYapp <kukiyomenaisapp@gmail.com>
2023-03-07 21:40:56 +0900

    Playerのガワ作成

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,17 @@
+namespace ActionGameTest
+{
+    public class Player
+    {
+        private readonly Input input;
+
+        public Player(Input input)
+        {
+            this.input = input;
+        }
+
+        public void Update()
+        {
+
+        }
+    }
+}
