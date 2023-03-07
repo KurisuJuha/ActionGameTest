cf5b60479817f8e3605544bfa21ce17fc4033d35
KYapp <kukiyomenaisapp@gmail.com>
2023-03-07 23:16:56 +0900

    必要なメンバーたち

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,17 +1,29 @@
 using System.Collections.ObjectModel;
 using ActionGameTest.Physics;
+using UnityEngine;
 
 namespace ActionGameTest
 {
     public class Player
     {
-        private readonly Input input;
+        public Vector2 position { get; private set; }
+        private readonly GameInput input;
         private readonly ReadOnlyCollection<Box> fieldBoxes;
+        private bool onCeiling;
+        private bool onGround;
+        private bool onLeftWall;
+        private bool onRightWall;
+        private Vector2 velocity;
 
-        public Player(Input input, Box[] fieldBoxes)
+        private const float gravityScale = 0.03f;
+        private const float moveSpeed = 0.2f;
+        private const float jumpPower = 0.4f;
+
+        public Player(GameInput input, Box[] fieldBoxes)
         {
             this.input = input;
             this.fieldBoxes = new(fieldBoxes);
+            position = new(-0.5f, -0.5f);
         }
 
         public void Update()
